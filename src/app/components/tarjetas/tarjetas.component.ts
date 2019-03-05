f962f5b7d7be575dfb1d8288d3d714db09f71cbe
Angel <mcinegas@gmail.com>
2019-03-05 19:28:48 +0100

    c tarjetas funcionando, ahora la info va por @input

---
+++ src/app/components/tarjetas/tarjetas.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjetas',
+  templateUrl: './tarjetas.component.html',
+  styles: []
+})
+export class TarjetasComponent {
+
+  @Input() items:any[] =[];
+
+  constructor() { }
+
+  
+
+}
