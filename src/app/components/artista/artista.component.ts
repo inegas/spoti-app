ce51edfda7bce3dbb794370ea7912857c757bd7b
Angel <mcinegas@gmail.com>
2019-03-07 20:00:10 +0100

    c artista funcionando, loading implementado

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from '../../services/spotify.service';
+
+
 
 @Component({
   selector: 'app-artista',
   templateUrl: './artista.component.html',
   styles: []
 })
-export class ArtistaComponent implements OnInit {
+export class ArtistaComponent {
 
-  constructor() { }
+  artist:any = {};
+  loading:boolean;
 
-  ngOnInit() {
+  constructor(private aroute:ActivatedRoute, private spotify:SpotifyService) {
+
+    this.loading = true;
+
+    this.aroute.params.subscribe(params =>{
+      console.log(params);
+      this.showArtist(params['id']);
+    });
+   }
+  
+   showArtist(id:string){
+      this.loading = true;
+      this.spotify.getArtist(id).subscribe(params =>{
+        console.log(params);
+        this.artist = params;
+        this.loading = false;
+      });
   }
-
 }
