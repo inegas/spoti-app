dbc131151dde2c257e796818b24750c59df2e431
Angel <mcinegas@gmail.com>
2019-03-13 17:11:19 +0100

    Pipe doomseguro, para que no pete la app con el widget y las url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { NoimagePipe } from './pipes/noimage.pipe';
 import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 
 
@@ -28,6 +29,7 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     ArtistaComponent,
     NavbarComponent,
     NoimagePipe,
+    DomseguroPipe,
     TarjetasComponent,
     LoadingComponent
   ],
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -18,7 +18,7 @@ export class SpotifyService {
      const url = `https://api.spotify.com/v1/${query}`;
 
      const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQB0mmnkmQgoiLnH0h7j2LPQOycdgZRAE0or-84_N7k41xSShgu4k-5fGW7HYezmQFVQXvKarO1Q_hh7Ac8'
+      'Authorization': 'Bearer BQDFmCJ4dxwzJCzbfX9mU2qxrot-kD4X97eU8sNtZVKsqXIwUhz9ixYmvAZdqnv1BLawXZ7T0TmhL3H_ecI'
     });
 
     return this.http.get(url, {headers});
